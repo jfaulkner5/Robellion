6f9f3e66a534d0a479195ac5955383cd13a8c1a4
SAE\dbuckley3 <1008669@student.sae.edu.au>
2017-07-11 13:17:02 +1000

    Created: ConveyorBelt.cs and Enemy.cs scripts. Started to write most of the main features for each of them.

---
+++ Assets/ConveyorBelt.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public ConveyorBeltType type;
+    public AxisOrientation axisOrientation;
+    public float speed = 1.0f;
+    public List<Enemy> curEnemies = new List<Enemy>();
+    public ConveyorBelt nextConveyorBelt;   //The conveyor belt that this one pushes objects onto.
+
+    //Returns the position that the enemy needs to move to on the next conveyor belt.
+    //Position also has the enemy's horizontal offset based on the conveyor belt orientation.
+    public Vector3 GetNextConveyorBeltPosition (float enemyHorizontalOffset)
+    {
+        Vector3 pos = nextConveyorBelt.transform.position;  //Set a base position for the next pos without the added offset.
+
+        //-------------------------------------------------------------------------------------
+        //Need to add a Y offset above when we have the models complete, so we know distances.
+        //-------------------------------------------------------------------------------------
+
+        //Is the next conveyor belt moving along a different axis direction than the current one?
+        if(axisOrientation != nextConveyorBelt.axisOrientation)
+        {
+            //Is the current conveyor belt moving along the X axis and the next one moving along the Z axis?
+            if(axisOrientation == AxisOrientation.X && nextConveyorBelt.axisOrientation == AxisOrientation.Z)
+            {
+                pos = new Vector3(pos.x + enemyHorizontalOffset, pos.y, pos.z);
+            }
+            else
+            {
+                pos = new Vector3(pos.x, pos.y, pos.z + enemyHorizontalOffset);
+            }
+        }
+        else
+        {
+            //Is the current conveyor belt moving along the X axis?
+            if(axisOrientation == AxisOrientation.X)
+            {
+                pos = new Vector3(pos.x, pos.y, pos.z + enemyHorizontalOffset);
+            }
+            else
+            {
+                pos = new Vector3(pos.x + enemyHorizontalOffset, pos.y, pos.z);
+            }
+        }
+
+        return pos;
+    }
+
+    void OnTriggerEnter (Collider col)
+    {
+        //If the enemy enters the conveyor belt trigger.
+        if(col.gameObject.tag == "Enemy")
+        {
+            //Get enemy script and set curConveyorBelt to this one.
+            //Add enemy to curEnemies.
+            //Set enemy speed to this conveyor belt's speed.
+        }
+    }
+
+    void OnTriggerExit (Collider col)
+    {
+        //If the enemy exits the conveyor belt trigger.
+        if(col.gameObject.tag == "Enemy")
+        {
+            //Remove enemy from curEnemies.
+        }
+    }
+}
+
+public enum ConveyorBeltType { Straight, Switcher }
+public enum AxisOrientation { X, Z }
---
+++ Assets/Enemy.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int curHealth;
+    public int maxHealth;
+
+    //Particle Effects
+    public ParticleSystem sparkParticleEffect;
+    public ParticleSystem smokeParticleEffect;
+    public ParticleSystem deathParticleEffect;
+
+    //Conveyor Belt
+    public ConveyorBelt curConveyorBelt;
+    private Vector3 positionToMoveTo;           //The position in the middle of the next conveyor belt.
+    public float horizontalOffsetOnConveyorBelt;    //The horizontal offset of the enemy on the conveyor belt.
+
+    void Update ()
+    {
+        //Move enemy to next position on the next conveyor belt.
+        if(Vector3.Distance(transform.position, positionToMoveTo) < 0.05f)
+        {
+            positionToMoveTo = curConveyorBelt.GetNextConveyorBeltPosition(horizontalOffsetOnConveyorBelt);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, positionToMoveTo, curConveyorBelt.speed * Time.deltaTime);
+        }
+    }
+
+    //This function gets called when the enemy takes damage.
+    //damageTaken, is the amount of damage that needs to be taken.
+    public void TakeDamage (int damageTaken)
+    {
+        if(curHealth - damageTaken <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            curHealth -= damageTaken;
+            CheckDamageParticleEffects();
+        }
+    }
+
+    //This function gets called when the enemy's health is less than or equals to 0.
+    //It destroys the enemy, as well as removing it from lists.
+    void Die ()
+    {
+        //Play and destroy the death particle effect.
+        deathParticleEffect.transform.parent = null;
+        deathParticleEffect.Play();
+        Destroy(deathParticleEffect.gameObject, deathParticleEffect.main.duration);
+
+        /*
+        - Remove from lists.
+        - Play death audio.
+        - If enemy type is molten metal, then call function is MoltenMetalRobot.cs.
+        */
+
+        Destroy(gameObject);
+    }
+
+    //This function gets called whent the enemy takes damage.
+    //It checks to see if the health is below a certain amount and plays damage particle effects if so.
+    void CheckDamageParticleEffects ()
+    {
+        //Check to see if we should start to emit the spark particle effect.
+        if(curHealth / maxHealth <= 0.5f)
+        {
+            sparkParticleEffect.Play();
+        }
+
+        //Check to see if we should start to emit the smoke particle effect.
+        if(curHealth / maxHealth <= 0.2f)
+        {
+            smokeParticleEffect.Play();
+        }
+    }
+}
