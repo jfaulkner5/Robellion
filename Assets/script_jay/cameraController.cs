521893af5d654f061bc3c32656b767ed3480acae
SAE\jfaulkner5 <1010100@student.sae.edu>
2017-07-11 13:57:37 +1000

    camera - sh*t broke - what even is a quaternian?

---
+++ Assets/script_jay/cameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+
+
+    //public GameObject cameraTarget;
+    //public Vector3 targetTempPos;
+    public Vector3 cameraOffset;
+    public Quaternion cameraOffsetQ;
+
+
+
+	// Use this for initialization
+	void Start () {
+        //cameraOffset = cameraTarget.transform.position - transform.position;
+        //Debug.Log(cameraOffset);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        //transform.position = cameraTarget.transform.position - cameraOffset;
+    }
+}
---
+++ Assets/script_jay/player_controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+    private Vector3 move;
+    private player_class p_c; //refers to the player_class script to get functions
+
+    void Start()
+    {
+        p_c = GetComponent<player_class>();
+    }
+
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        // jump = Input.GetButton("Jump");
+        move = (v * Vector3.forward + h * Vector3.right).normalized;
+
+    }
+
+    private void FixedUpdate()
+    {
+        // Call the Move function of the player controller
+        p_c.Move(move);
+
+    }
+}
