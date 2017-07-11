ecc11bbe391aa59db4dd696475656b6b9a4975ff
steph <1006362@student.sae.edu>
2017-07-11 13:04:16 +1000

    Added GameManager script Added enemy tag to enemy prefabs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    int health;
+
+    float waveTimer = 5f; //adjust time later
+    float timer;
+
+    public GameManager instance = null;
+
+    int enemiesLeft;
+
+    private void Awake()
+    {
+        //check if instance already exists
+        if (instance == null)
+            
+            //if not, set to this
+            instance = this;
+
+        //if instance exists but isnt this: destroy it
+        else if (instance != this)
+            Destroy(gameObject);
+
+        //reloading the scene wont destroy it
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= waveTimer)
+        {
+            /* either start wave spawning or stop wave spawning
+             * not sure which we're doing here*/
+        }
+
+        //find game objects with the enemy tag
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        enemiesLeft = enemies.Length;
+    }
+}
