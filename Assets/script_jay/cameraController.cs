ab2ec3141211dbc50d5bdf03209de95917988d33
SAE\jfaulkner5 <1010100@student.sae.edu>
2017-07-19 14:54:47 +1000

    - fixed camera rotation around 'target' testing functionality for ability to emulate touch input without deployment to device each time

--- Assets/script_jay/cameraController.cs
+++ Assets/script_jay/cameraController.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 
 public class cameraController : MonoBehaviour {
 
-
-    //public GameObject cameraTarget;
-    //public Vector3 targetTempPos;
-    public Vector3 cameraOffset;
-    public Quaternion cameraOffsetQ;
-
+    public GameObject target;//the target object
+    public float viewAngle = 40; //angle to look down at target
+    public float speedMod = 10.0f;//a speed modifier
+    private Vector3 targetLoc;//the coord to the point where the camera looks at
 
 
-	// Use this for initialization
-	void Start () {
-        //cameraOffset = cameraTarget.transform.position - transform.position;
-        //Debug.Log(cameraOffset);
-		
-	}
+    // Use this for initialization
+    void Start () {
+        targetLoc = target.transform.position;  //get target's coords
+        transform.LookAt(targetLoc);    //makes the camera look to it
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        //transform.RotateAround(targetLoc, new Vector3(0.0f, 1.0f, 0.0f), viewAngle * Time.deltaTime * speedMod);
+        if (Input.GetKey("a"))
+        transform.RotateAround(target.transform.position, Vector3.up, -speedMod * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+            transform.RotateAround(target.transform.position, Vector3.up, speedMod * Time.deltaTime);
+
+
+
+    }
 
     private void LateUpdate()
     {
