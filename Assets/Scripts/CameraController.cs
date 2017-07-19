dee4cde754f7751e173e4f5296f1c3370b745a9a
Jay Faulkner <1010100@student.sae.edu.au>
2017-07-20 00:38:46 +1000

    Finsihed Main Menu placeholder credits info added temp 'trainer controls' to camera script for showoff | use 1/2/3 to load respective levels

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+#if UNITY_EDITOR
+//cheatcode
+using UnityEngine.SceneManagement;
+#endif
+
+
+
 public class CameraController : MonoBehaviour
 {
 
@@ -26,37 +34,44 @@ public class CameraController : MonoBehaviour
         //rotates the camera rig around focused object
         if (Input.GetKey("a"))
             camRig.transform.RotateAround(target.transform.position, Vector3.up, -speedMod * Time.deltaTime);
-
         if (Input.GetKey("d"))
             camRig.transform.RotateAround(target.transform.position, Vector3.up, speedMod * Time.deltaTime);
-
         // input checks for zoom in/out of cam in reference to local pos (pos related to parent)
         if (Input.GetKey ("w"))
 		{
             tempPos = transform.localPosition;
 			transform.localPosition = tempPos + Vector3.forward  * speedMod * Time.deltaTime;
-		}
-
-		if (Input.GetKey ("s"))
+            if (transform.localPosition.z >= 3)
+                transform.localPosition = new Vector3(0, 0, 3);
+        }
+        if (Input.GetKey ("s"))
 		{
 			tempPos = transform.localPosition;
 			transform.localPosition = tempPos + Vector3.back * speedMod * Time.deltaTime;
-		}
-
-        if (transform.localPosition.z >= 3)
-            transform.localPosition =  new Vector3(0,0,3);
-        if (transform.localPosition.z <= -5)
-            transform.localPosition = new Vector3(0, 0, -5);
+            if (transform.localPosition.z <= -5)
+                transform.localPosition = new Vector3(0, 0, -5);
+        }
 
 
 
+        // cheat code
+        if (Input.GetKey("1"))
+        {
+            SceneManager.LoadScene(1);
+            Debug.Log("scene loaded 1");
+        }
+        if (Input.GetKey("2"))
+        {
+            SceneManager.LoadScene(2);
+            Debug.Log("scene loaded 2");
+        }
+            
+        if (Input.GetKey("3"))
+        {
+            SceneManager.LoadScene(3);
+            Debug.Log("scene loaded 3");
+        }
 
-        /*test code
-		fov = Camera.main.fieldOfView;
-		fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
-		fov = Mathf.Clamp(fov, minFov, maxFov);
-		Camera.main.fieldOfView = fov;
-		*/
 
 
     }
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif 
+
+public class MenuManager : MonoBehaviour {
+
+    public GameObject MainMenu; //Main menu || menu that is displayed by default
+    public GameObject OtherMenu; //menu that is toggled off by default
+    public int lvlSelect; //int for scene selector. allows multiple uses in different scenes
+
+
+    //loads scene dependant on int set in editor
+    public void LevelStart()
+    {
+        SceneManager.LoadScene(lvlSelect);
+
+    }
+
+
+    // Used to toggle menu panels
+    public void MenuToggle()
+    {
+        if (MainMenu.activeSelf)
+        {
+            MainMenu.SetActive(false);
+            OtherMenu.SetActive(true);
+        }
+        else
+        {
+            MainMenu.SetActive(true);
+            OtherMenu.SetActive(false);
+        }
+    }
+
+    //Quits from application
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   
+}
