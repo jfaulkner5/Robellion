1b6b871409e738e1351f8bbca11003530465f2e7
SAE\dbuckley3 <1008669@student.sae.edu.au>
2017-07-20 14:18:34 +1000

    Made the in-game UI look nicer.

--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -9,13 +9,22 @@ public class GameUI : MonoBehaviour
 	public Text curWaveText;
 	public Text curHealthText;
 	public Text timeText;
+	public Slider healthBar;
+
+	void Start ()
+	{
+		healthBar.maxValue = GameManager.gm.health;
+	}
 
 	void Update ()
 	{
-		scrapText.text = "Scrap\n<size=50>" + GameManager.gm.curScrap + "</size>";
-		curHealthText.text = "Health\n<size=50>" + GameManager.gm.health + "</size>";
-		curWaveText.text = "Wave\n<size=50>" + GameManager.gm.curWave + "</size>";
+		scrapText.text = GameManager.gm.curScrap.ToString();
+		curHealthText.text = GameManager.gm.health + " Lives";
+		curWaveText.text = "Wave " + GameManager.gm.curWave;
+		healthBar.value = GameManager.gm.health;
 
+		//If the wave is active, then display the time elapsed so far into the wave.
+		//If the wave is done, then display the time remaining before next wave.
 		if(GameManager.gm.curGameState == GameState.WaveActive)
 		{
 			timeText.text = "Time Elapsed\n<size=50>" + (int)GameManager.gm.waveTime + "s</size>";
