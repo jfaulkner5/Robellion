115f7cfe8943824245e29fbb5c70919dda42a3ae
Steph <1006462@student.sae.edu>
2017-08-02 16:37:10 +1000

    Added drill tower, drill tower type, and piercing damage type

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -8,7 +8,8 @@ public enum DamageType
     Melee,
     Heat,
     Electricity,
-    Acid
+    Acid,
+    Piercing
 }
 
 /// <summary>
@@ -122,7 +123,7 @@ public class Tower : MonoBehaviour
     }
 }
 
-public enum TowerType { RobotArm, Crusher, Lazer, AcidEtcher }
+public enum TowerType { RobotArm, Crusher, Lazer, AcidEtcher, Drill }
 
 /// <summary>
 /// 
