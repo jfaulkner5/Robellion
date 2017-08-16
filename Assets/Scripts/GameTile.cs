b04a9d8781fc73348d12869d8e14a42c9f1b8f1d
Mithi <mithoyd@gmail.com>
2017-08-16 16:20:47 +1000

    Added script for tiling GameTiles and added it to the prefab

---
+++ Assets/Scripts/GameTile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTile : MonoBehaviour {
+
+    public Material GameTileMaterial;
+    public Material AltGameTileMaterial;
+
+    public MeshRenderer meshRenderer;
+
+	// Use this for initialization
+	void Start () {
+		if(transform.position.x % 2 == transform.position.z %2 || transform.position.x % 2 == -transform.position.z % 2)
+        {
+            meshRenderer.material = GameTileMaterial;
+        }
+        else
+        {
+            meshRenderer.material = AltGameTileMaterial;
+        }
+	}
+}
