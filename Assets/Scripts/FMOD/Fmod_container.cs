60a8a36b7ea53246b31290821f7f91627ee6f7eb
Jay Faulkner <jfaulkner@protonmail.ch>
2018-05-07 00:14:48 +1000

    fmod audio initial

---
+++ Assets/Scripts/FMOD/Fmod_container.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FMOD_hotfixes
+{
+    public class Fmod_container : MonoBehaviour
+    {
+        [FMODUnity.EventRef] public string eventRefence;
+
+        public FMOD.Studio.EventInstance eventInstance;
+
+    }
+}
