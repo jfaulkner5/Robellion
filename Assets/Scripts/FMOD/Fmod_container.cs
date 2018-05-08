2b1dbf2ed9844e5fc88520a86c37b42cc3cbc227
Jay Faulkner <jfaulkner@protonmail.ch>
2018-05-08 16:43:45 +1000

    fixed FMOD custom classes showing in inspector properly

--- Assets/Scripts/FMOD/Fmod_container.cs
+++ Assets/Scripts/FMOD/Fmod_container.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 namespace FMOD_hotfixes
 {
-    public class Fmod_container : MonoBehaviour
-    {
-        [FMODUnity.EventRef] public string eventRefence;
 
+
+    [System.Serializable]
+    public class Fmod_container //: UnityEditor.PropertyDrawer
+    {
+       
+        [FMODUnity.EventRef] public string eventRefence;
         public FMOD.Studio.EventInstance eventInstance;
 
+        public void Awake()
+        {
+            eventInstance = FMODUnity.RuntimeManager.CreateInstance(eventRefence);
+        }
     }
+
+
+    //Unsure if this is neccesary...
+
+    //public class FMOD_eventpack : MonoBehaviour
+    //{
+    //    public Fmod_container test;
+
+    //}
+
 }
