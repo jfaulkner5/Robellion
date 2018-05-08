529eae380dd415f4b46dfa037f54ded02cc02503
Jay Faulkner <jfaulkner@protonmail.ch>
2018-05-08 16:43:13 +1000

    ignore
    attempt at custom inspector for FMOD

---
+++ Assets/Scripts/FMOD/FMOD_inspector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace FMOD_hotfixes
+{
+    //[UnityEditor.CustomPropertyDrawer(typeof(Fmod_container))]
+    //public class FMOD_inspector : PropertyDrawer
+    //{
+
+    //}
+
+
+
+    //public class NewEditorScript1 : ScriptableObject
+    //{
+    //    [MenuItem("Tools/MyTool/Do It in C#")]
+    //    static void DoIt()
+    //    {
+    //        EditorUtility.DisplayDialog("MyTool", "Do It in C# !", "OK", "");
+    //    }
+    //}
+}
